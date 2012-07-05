a8b39da392982719919a714e959633cd21fc9c30
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2012-07-05 15:09:38 +0100

    add install script

---
+++ bin/install.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+require 'fileutils'
+SKIP_PATTERNS=[/^\./, /^bin$/, /^lib$/]
+install_dir = File.expand_path(File.dirname(__FILE__))
+home = ENV['HOME']
+Dir.new(install_dir).each do |filename|
+ home_file_name =  File.join(home, ".#{filename}")
+ checked_in_file_name = File.join(install_dir, filename)
+ FileUtils.ln_sf(checked_in_file_name,home_file_name) unless SKIP_PATTERNS.map {|pattern| filename.match pattern}.compact.any? ||File.exists?(home_file_name)
+end
+
+
